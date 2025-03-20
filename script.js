29e74b7e54772a1df1a037a24c86347b39c43d8b
vela0474 <vela0474@student.acitech.org>
2025-03-20 08:56:46 -0400

    Nothing quite chnaged, fixed couple things

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
 const your_pick = document.querySelectorAll (".your_pick");
 const score_boardEl = document.getElementById ("computer_score");
-const scoreEl = document.getElementById ("user_score");
+const user_scoreEl = document.getElementById ("user_score");
 const reset = document.getElementById ("reset");
-const resulttext = document.getElementById ("result");
+const result_text = document.getElementById ("result");
 
 
 let user_score = 0;
 let computer_score = 0;
 
+
 your_pick.forEach(your_pick => {
     your_pick.addEventListener("click", () => {
-        const user_score = your_pick.id;
         playGame(your_pick);
+        const user_score = your_pick.id;
     });
-    
 });
 
-function playGame (your_pick) {
+
+function playGame (useryour_pick) {
+    const useryour_pick = 
+    getusueryour_pick();
     const computeryour_pick = 
     getcomputeryour_pick(); 
-
    const winner = determinewinner(useryour_pick, computeryour_pick);
-   
    updateUI(useryour_pick, computeryour_pick, winner);
-
 }
 
 function getcomputeryour_pick() {
     const options = ["rock","paper","scissor"];
+    const randomIndex = Math.floor(Math.random() * options.length);
 
-    const randomindex = math.floor(math.random() * options.length);
-
-    return options[randomindex];
+    return options[randomIndex];
 
 }
 
@@ -54,19 +53,19 @@ if (
     }
 function updateUI(useryour_pick, computeryour_pick, winner) {
     if (winner === "draw") {
-        resulttext.textcontent = `It ends in a draw ${useryour_pick}.`;
+        result_text.textcontent = `It ends in a draw ${useryour_pick}.`;
     } else if (winner === "winner") {
-        resulttext.textContent = `You win ${useryour_pick} beats ${computeryour_pick}.`;
+        result_text.textContent = `You win ${useryour_pick} beats ${computeryour_pick}.`;
         user_scoreEl.textContent = user_score;
     } else {
-        resulttext.textContent = `You loose ${computeryour_pick} beats ${useryour_pick}.`;
-        computer_scoreEl.textContent = computer_score;
+        result_text.textContent = `You loose ${computeryour_pick} beats ${useryour_pick}.`;
+        score_boardEl.textContent = computer_score;
     }
 }
 
 reset.addEventListener("click", () => {
     user_score = 0;
     computer_score = 0;
-    user_score.textContent = "0";
-    computer_score.textContent = "0";
+    user_scoreEl.textContent = "0";
+    score_boardEl.textContent = "0";
 });
