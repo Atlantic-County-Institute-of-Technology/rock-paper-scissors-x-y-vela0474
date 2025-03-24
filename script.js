def1aa3d686eae9f89c1e996fe1ecb19a17e4ba3
vela0474 <vela0474@student.acitech.org>
2025-03-24 09:12:54 -0400

    My buttons are working, there just broken.

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const your_pick = document.querySelectorAll (".your_pick");
 const score_boardEl = document.getElementById ("computer_score");
 const user_scoreEl = document.getElementById ("user_score");
-const reset = document.getElementById ("reset");
+const reset_button = document.getElementById("reset_button");
 const result_text = document.getElementById ("result_text");
-
 
 let user_score = 0;
 let computer_score = 0;
@@ -11,31 +10,21 @@ let computer_score = 0;
 
 your_pick.forEach(your_pick => {
     your_pick.addEventListener("click", () => {
-        user_score = your_pick.id;
+    const user_score = your_pick.id;
         playGame(user_score);
     });
 });
 
 
-function playGame () {
-    const useryour_pick = 
-    getusueryour_pick();
-    const computeryour_pick = 
-    getcomputeryour_pick(); 
+function playGame (useryour_pick) {
+    const computeryour_pick =
+    getcomputeryour_pick();
    const winner = determinewinner(useryour_pick, computeryour_pick);
    updateUI(useryour_pick, computeryour_pick, winner);
 }
 
-function user_score() {
-    const options = ["rock", "paper", "scissor", "pencil", "lipstick"];
-    const randomIndex = Math.floor(Math.random() * options.length);
-   
-    return options[randomIndex];
-
-}
-
 function getusueryour_pick() {
-    const options = ["rock", "paper", "scissor", "pencil", "lipstick"];
+    const options = ["rock","paper","scissor","pencil","lipstick"];
     const randomIndex = Math.floor(Math.random() * options.length);
    
     return options[randomIndex];
@@ -43,7 +32,7 @@ function getusueryour_pick() {
 }
 
 function getcomputeryour_pick() {
-    const options = ["rock","paper","scissor", "pencil", "lipstick"];
+    const options = ["rock","paper","scissor","pencil","lipstick"];
     const randomIndex = Math.floor(Math.random() * options.length);
     
     return options[randomIndex];
@@ -57,16 +46,26 @@ function determinewinner(useryour_pick, computeryour_pick) {
 
 if (
     (useryour_pick === "rock" && computeryour_pick === "scissor") ||
+
     (useryour_pick === "paper" && computeryour_pick === "rock") ||
+
     (useryour_pick === "scissor" && computeryour_pick === "paper") ||
-    (useryour_pick === "pencil"  && computeryour_pick === "paper") ||
+
+    (useryour_pick === "pencil" && computeryour_pick === "paper") ||
+
     (useryour_pick === "pencil" && computeryour_pick === "lipstick") ||
+
     (useryour_pick === "rock" && computeryour_pick === "pencil") ||
+
     (useryour_pick === "scissor" && computeryour_pick === "pencil") ||
+
     (useryour_pick === "lipstick" && computeryour_pick === "paper") ||
+
     (useryour_pick === "lipstick" && computeryour_pick === "scissor") ||
+
     (useryour_pick === "rock" && computeryour_pick === "lipstick") ||
-    (useryour_pick === "scissor" && computeryour_pick === "lipstick")
+    
+    (useryour_pick === "scissor" && computeryour_pick === "lipstick") 
 ) {
     user_score++;
     return "user";
@@ -75,10 +74,11 @@ if (
     return "computer";
 }
     }
+
 function updateUI(useryour_pick, computeryour_pick, winner) {
     if (winner === "draw") {
         result_text.textContent = `It ends in a draw ${useryour_pick}.`;
-    } else if (winner === "winner") {
+    } else if (winner === "user") {
         result_text.textContent = `You win ${useryour_pick} beats ${computeryour_pick}.`;
         user_scoreEl.textContent = user_score;
     } else {
@@ -87,9 +87,10 @@ function updateUI(useryour_pick, computeryour_pick, winner) {
     }
 }
 
-reset.addEventListener("click", () => {
+ reset_button.addEventListener("click", () => {
     user_score = 0;
     computer_score = 0;
     user_scoreEl.textContent = "0";
     score_boardEl.textContent = "0";
-});
+    result_text.textContent = "Choose your weapon";
+})
