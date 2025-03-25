796c1b2c826e8a3a4c79bd521787be0d26e413cf
vela0474 <vela0474@student.acitech.org>
2025-03-25 09:09:24 -0400

    Still no chnages, need help please.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const your_pick = document.querySelectorAll (".your_pick");
-const score_boardEl = document.getElementById ("computer_score");
 const user_scoreEl = document.getElementById ("user_score");
+const computer_scoreEl = document.getElementById ("computer_score");
 const reset_button = document.getElementById("reset_button");
 const result_text = document.getElementById ("result_text");
 
@@ -21,15 +21,7 @@ function playGame (useryour_pick) {
     getcomputeryour_pick();
    const winner = determinewinner(useryour_pick, computeryour_pick);
    updateUI(useryour_pick, computeryour_pick, winner);
-}
-
-function getusueryour_pick() {
-    const options = ["rock","paper","scissor","pencil","lipstick"];
-    const randomIndex = Math.floor(Math.random() * options.length);
-   
-    return options[randomIndex];
-
-}
+};
 
 function getcomputeryour_pick() {
     const options = ["rock","paper","scissor","pencil","lipstick"];
@@ -37,60 +29,66 @@ function getcomputeryour_pick() {
     
     return options[randomIndex];
 
-}
+};
 
 function determinewinner(useryour_pick, computeryour_pick) {
     if (useryour_pick === computeryour_pick) {
         return "draw";
-}
+};
 
 if (
-    (useryour_pick === "rock" && computeryour_pick === "scissor") ||
+   (useryour_pick === "scissor" && computeryour_pick === "lipstick") ||
 
-    (useryour_pick === "paper" && computeryour_pick === "rock") ||
+   (useryour_pick === "rock" && computeryour_pick === "pencil") ||
 
-    (useryour_pick === "scissor" && computeryour_pick === "paper") ||
+   (useryour_pick === "pencil" && computeryour_pick === "lipstick") ||
 
-    (useryour_pick === "pencil" && computeryour_pick === "paper") ||
+   (useryour_pick === "lipstick" && computeryour_pick === "paper") ||
 
-    (useryour_pick === "pencil" && computeryour_pick === "lipstick") ||
+   (useryour_pick === "rock" && computeryour_pick === "lipstick") ||
 
-    (useryour_pick === "rock" && computeryour_pick === "pencil") ||
+   (useryour_pick === "rock" && computeryour_pick === "scissor") ||
 
-    (useryour_pick === "scissor" && computeryour_pick === "pencil") ||
+   (useryour_pick === "scissor" && computeryour_pick === "paper") ||
 
-    (useryour_pick === "lipstick" && computeryour_pick === "paper") ||
+   (useryour_pick === "pencil" && computeryour_pick === "paper") ||
 
-    (useryour_pick === "lipstick" && computeryour_pick === "scissor") ||
+   (useryour_pick === "paper" && computeryour_pick === "rock") ||
 
-    (useryour_pick === "rock" && computeryour_pick === "lipstick") ||
-    
-    (useryour_pick === "scissor" && computeryour_pick === "lipstick") 
-) {
+   (useryour_pick === "lipstick" && computeryour_pick === "scissor") ||
+
+   (useryour_pick === "scissor" && computeryour_pick === "paper") 
+
+) 
+
+{
     user_score++;
     return "user";
-} else {
+} 
+    else {
     computer_score++;
     return "computer";
 }
-    }
+    };
 
-function updateUI(useryour_pick, computeryour_pick, winner) {
-    if (winner === "draw") {
-        result_text.textContent = `It ends in a draw ${useryour_pick}.`;
-    } else if (winner === "user") {
-        result_text.textContent = `You win ${useryour_pick} beats ${computeryour_pick}.`;
-        user_scoreEl.textContent = user_score;
-    } else {
-        result_text.textContent = `You lose ${computeryour_pick} beats ${useryour_pick}.`;
-        score_boardEl.textContent = computer_score;
-    }
-}
+    function updateUI(useryour_pick, computeryour_pick, winner) {
+        if (winner === "draw") {
+            result_text.textContent = `It ends in a draw ${useryour_pick}`;
+        } else if (winner === "computer") {
+            result_text.textContent = `You win ${useryour_pick} beats ${computeryour_pick}`;
+            user_scoreEl.textContent = user_score;
+           
+        } else {
+            result_text.textContent = `You lose ${computeryour_pick} beats ${useryour_pick}`;
+            computer_scoreEl.textContent = computer_score;
+        }
+    };
+    
 
  reset_button.addEventListener("click", () => {
     user_score = 0;
     computer_score = 0;
     user_scoreEl.textContent = "0";
-    score_boardEl.textContent = "0";
+    computer_scoreEl.textContent = "0";
     result_text.textContent = "Choose your weapon";
-})
+});
