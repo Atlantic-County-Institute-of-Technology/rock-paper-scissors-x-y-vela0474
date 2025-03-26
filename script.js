97ab35cf45d164a1c2f26a4d7c025c7114c7b7dd
vela0474 <vela0474@student.acitech.org>
2025-03-26 08:33:22 -0400

    I am done, everything works.

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let computer_score = 0;
 your_pick.forEach(your_pick => {
     your_pick.addEventListener("click", () => {
     const user_score = your_pick.id;
-        playGame(user_score);
+        playGame(user_score);        
     });
 });
 
@@ -19,8 +19,8 @@ your_pick.forEach(your_pick => {
 function playGame (useryour_pick) {
     const computeryour_pick =
     getcomputeryour_pick();
-   const winner = determinewinner(useryour_pick, computeryour_pick);
-   updateUI(useryour_pick, computeryour_pick, winner);
+    const winner = determinewinner(useryour_pick, computeryour_pick);
+    updateUI(useryour_pick, computeryour_pick, winner);
 };
 
 function getcomputeryour_pick() {
@@ -28,53 +28,40 @@ function getcomputeryour_pick() {
     const randomIndex = Math.floor(Math.random() * options.length);
     
     return options[randomIndex];
-
+    
+    
 };
 
 function determinewinner(useryour_pick, computeryour_pick) {
+    // if the user and cpu are the same, draw
     if (useryour_pick === computeryour_pick) {
         return "draw";
-};
+    } 
+    // all win conditions for user
+    else if (
+    (useryour_pick === "scissor" && computeryour_pick === "lipstick" || computeryour_pick === "paper" || computeryour_pick === "pencil") ||
 
-if (
-   (useryour_pick === "scissor" && computeryour_pick === "lipstick") ||
+    (useryour_pick === "rock" && computeryour_pick === "pencil" || computeryour_pick === "lipstick" || computeryour_pick === "scissor") ||
 
-   (useryour_pick === "rock" && computeryour_pick === "pencil") ||
+    (useryour_pick === "pencil" && computeryour_pick === "lipstick" || computeryour_pick === "paper" ) ||
 
-   (useryour_pick === "pencil" && computeryour_pick === "lipstick") ||
+    (useryour_pick === "lipstick" && computeryour_pick === "paper" || computeryour_pick === "scissor") ||
 
-   (useryour_pick === "lipstick" && computeryour_pick === "paper") ||
-
-   (useryour_pick === "rock" && computeryour_pick === "lipstick") ||
-
-   (useryour_pick === "rock" && computeryour_pick === "scissor") ||
-
-   (useryour_pick === "scissor" && computeryour_pick === "paper") ||
-
-   (useryour_pick === "pencil" && computeryour_pick === "paper") ||
-
-   (useryour_pick === "paper" && computeryour_pick === "rock") ||
-
-   (useryour_pick === "lipstick" && computeryour_pick === "scissor") ||
-
-   (useryour_pick === "scissor" && computeryour_pick === "paper") 
-
-) 
-
-{
-    user_score++;
-    return "user";
-} 
+    (useryour_pick === "paper" && computeryour_pick === "rock"))
+    {
+        user_score++;
+        return "user";
+    } 
+    // else, cpu wins
     else {
-    computer_score++;
-    return "computer";
-}
-    };
+        computer_score++;
+        return "computer";
+    }};
 
     function updateUI(useryour_pick, computeryour_pick, winner) {
         if (winner === "draw") {
             result_text.textContent = `It ends in a draw ${useryour_pick}`;
-        } else if (winner === "computer") {
+        } else if (winner === "user") {
             result_text.textContent = `You win ${useryour_pick} beats ${computeryour_pick}`;
             user_scoreEl.textContent = user_score;
            
@@ -82,7 +69,7 @@ if (
             result_text.textContent = `You lose ${computeryour_pick} beats ${useryour_pick}`;
             computer_scoreEl.textContent = computer_score;
         }
-    };
+    }; /*All of this makes the game end in a draw, win, or lose for the cpu or user*/
     
 
  reset_button.addEventListener("click", () => {
@@ -91,4 +78,4 @@ if (
     user_scoreEl.textContent = "0";
     computer_scoreEl.textContent = "0";
     result_text.textContent = "Choose your weapon";
-});
+}); /*This resets the points of the game and restarts the whole game to 0*/
